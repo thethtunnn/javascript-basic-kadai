f119aa34ab4ebc07729873119762d645c0f1463d
THET HTUN <87650163+thethtunnn@users.noreply.github.com>
2024-06-07 15:39:30 +0900

    Add files via upload
    za

---
+++ kadai_007/calc.js
@@ -0,0 +1,33 @@
+
+// const num = 363;
+
+
+// if (num % 3 === 0 && num % 5 === 0 ) {
+//   console.log( "3と5の倍数です");
+// }else if (num % 3 === 0) {
+//     console.log("3の倍数です");
+//   }else if (num % 5 === 0) {
+//     console.log( "5の倍数です");
+//   }else{
+//     console.log(num);
+//   }
+  
+  
+
+let num  = 369;
+
+
+if (num > 0){  
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log('3と5の倍数です')
+    }
+    else if (num % 3 === 0) {      
+    console.log('3の倍数です');
+    }
+    else if(num % 5 === 0) {
+    console.log('5の倍数です');
+    }
+    else {
+    console.log(num)
+    }
+}
